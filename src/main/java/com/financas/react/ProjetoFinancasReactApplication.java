812c954c36a5be7ca21a9c6cd1d9891ca6545826
Marcio Grecco <marcio.greco@outlook.com>
2021-09-21 12:48:08 -0300

    Project created

---
+++ src/main/java/com/financas/react/ProjetoFinancasReactApplication.java
@@ -0,0 +1,13 @@
+package com.financas.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinancasReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinancasReactApplication.class, args);
+	}
+
+}
